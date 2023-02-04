47064b3f26d22842aaa70757f684ab0483d88c5f
btsutskiridze <bakatsutskiridze@redberry.ge>
2023-02-04 23:01:25 +0400

    feat: message *playerMove - display player move *computerMove - calculates and displays AI optimal move *handleMove - either displays winner or continues the game *functions to reset the game

--- src/modules/Game.ts
+++ src/modules/Game.ts
@@ -1,3 +1,7 @@
+import { checkWinner } from './helpers/check-result';
+import { displayResult, addSvg, humanClickWatcher } from './helpers/display';
+import { bestMove } from './helpers/minimax';
+
 class Game {
   private board: string[][];
   private currentPlayer: string;
@@ -13,6 +17,70 @@ class Game {
     this.currentPlayer = 'X';
     Game.AI = ai;
     Game.HUMAN = human;
+  }
+
+  public start(): void {
+    if (this.currentPlayer == Game.AI) this.computerMove();
+    humanClickWatcher(this);
+  }
+
+  public playerMove(row: number, col: number): void {
+    if (this.board[row][col] == '') {
+      this.board[row][col] = this.currentPlayer;
+      addSvg(row.toString() + col.toString(), Game.HUMAN);
+
+      this.handleMove(Game.HUMAN);
+    }
+  }
+  public computerMove(): void {
+    let move = bestMove(this.board);
+    this.board[move.i][move.j] = Game.AI;
+    addSvg(move.i.toString() + move.j.toString(), Game.AI);
+
+    this.handleMove(Game.AI);
+  }
+
+  private handleMove(player: string): void {
+    let winner = checkWinner(this.board);
+    if (winner == null) {
+      this.setPlayer();
+      if (player == Game.HUMAN) this.computerMove();
+      return;
+    }
+    displayResult(winner);
+    this.playAgain();
+  }
+
+  private setPlayer(): void {
+    this.currentPlayer = this.currentPlayer == 'X' ? 'O' : 'X';
+  }
+
+  private resetGame() {
+    document.getElementById('popup')?.remove();
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+    this.currentPlayer = 'X';
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const cell = document.getElementById(i.toString() + j.toString());
+        if (cell) {
+          cell.innerHTML = ``;
+        }
+      }
+    }
+  }
+
+  private playAgain() {
+    const btn = document.getElementById('play-again');
+
+    btn?.addEventListener('click', () => {
+      this.resetGame();
+      this.start();
+    });
   }
 }
 export default Game;
